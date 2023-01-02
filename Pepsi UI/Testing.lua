bebf40b1b7c580bfb4278048fb5df50a97d168c2
Kratato <partel.kaukver@gmail.com>
2023-01-02 15:15:04 +0200

    Updates and stuff
    Added Functions & Options, deleted an unnecessary file, updated AttritionVehicleEsp-Highlight.lua to actually work, and worked on Testing.lua

--- Pepsi UI/Testing.lua
+++ Pepsi UI/Testing.lua
@@ -1,5 +1,11 @@
 local Library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
 
+local function AddDivider(Path)
+	local Divider = Path:AddLabel({Name="------------------------------"})
+end
+
+
+
 local Window = Library:CreateWindow({
     Name = "Attrition",
     Themeable = {
@@ -11,21 +17,25 @@ local Tab = Window:CreateTab({
     Name = "General"
 })
 
-local Section = Tab:CreateSection({
-    Name = "Hax"
-})
+local VisualSection = Tab:CreateSection({Name="Visual",Side="Left"})
+local MovementSection = Tab:CreateSection({Name="Movement",Side="Right"})
+local MiscSection = Tab:CreateSection({Name="Miscellaneous",Side="Right"})
 
-Section:AddButton({
-    Name = "Test",
+local PlayerEspToggle = VisualSection:AddToggle({
+    Name = "PlayerEsp",
+    Flag = "PlayerEspToggle"
+})
+AddDivider(VisualSection)
+
+
+
+
+local MovementButton = MovementSection:AddButton({
+    Name = "Balls",
     Callback = function() end
 })
 
-Section:AddButton({
-    Name = "Test",
-    Callback = function() end
-})
-
-Section:AddButton({
-    Name = "Test",
+local MiscButton = MiscSection:AddButton({
+    Name = "Balls",
     Callback = function() end
 })
--- Testing/AttritionFindTeam.lua
+++
@@ -1,5 +0,0 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-repeat print("waiting for HUD...") wait(1) until LocalPlayer.PlayerGui.HudApp.MenuHud:FindFirstChild("Container") ~= nil
-print("Found! Team is:",LocalPlayer.PlayerGui.HudApp.MenuHud.Container.Leaderboard.PlayerTeam.TeamBanner.TeamName.Text)
